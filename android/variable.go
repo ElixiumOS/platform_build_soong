d35019a26fbe3f65d7101a8c700dda20e17d5198
Sean hoyt <seanhoyt963@gmail.com>
2017-08-23 15:51:54 +0000

    Add LINKER_FORCED_SHIM_LIBS to Soong
    Change-Id: Ie81e5e1ae56861580eefa2a3659e7146a958770f

--- android/variable.go
+++ android/variable.go
@@ -161,6 +161,8 @@ type productVariables struct {
 
 	BtConfigIncludeDir *string `json:",omitempty"`
 
+	ForcedShimLibs *string `json:",omitempty"`
+
 	Override_rs_driver *string `json:",omitempty"`
 
 	Libart_img_base            *string `json:",omitempty"`
